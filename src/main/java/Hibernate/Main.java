64f581c7b71b6d1ee588fd4e9db15d4e3226e8c1
Klodjan Mata <klodjanmata0@gmail.com>
2025-03-16 12:05:44 +0100

    lattest changes

--- src/main/java/Hibernate/Main.java
+++ src/main/java/Hibernate/Main.java
@@ -7,7 +7,13 @@ public class Main {
     public static void main(String[] args) {
         MovieRepository mr = new MovieRepository();
         Movie movie = new Movie();
-        List<Movie> moviesList = mr.findAll();
+        List<Movie> moviesList;
+        movie.setTitle("Perralle nga e kaluara");
+        movie.setGenre("Comedy");
+        movie.setYear(1980);
+        movie.setRating(100);
+        mr.save(movie);
+        moviesList = mr.findAll();
         for (Movie m : moviesList) {
             System.out.println(m);
         }
